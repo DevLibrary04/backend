2917494331304a69415348e253647347257af3ba
library04 <w1e1007@naver.com>
2025-06-17 11:12:24 +0900

    0617 db연동

--- src/main/java/edu/pnu/controller/HistoryController.java
+++ src/main/java/edu/pnu/controller/HistoryController.java
@@ -1,8 +1,6 @@
 package edu.pnu.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,37 +8,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.pnu.domain.History;
+import edu.pnu.domain.Member;
+import edu.pnu.dto.HistoryDTO;
+import edu.pnu.persistence.MemberRepository;
 import edu.pnu.service.HistoryService;
 import edu.pnu.util.JWTUtil;
-import edu.pnu.dto.HistoryDTO;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-//@Getter
-//@Setter
-//@ToString
-//class HistoryDTO {
-//	private String userId;
-//	private String query;
-//	private List<ResultDTO> results = new ArrayList<>();
-//}
-//
-//@Getter
-//@Setter
-//@ToString
-//class ResultDTO {
-//	private String title;
-//	private String originallink;
-//	private String description;
-//	private String link;
-//	private String pubDate;
-//}
 
 @RestController
 @RequestMapping("/api/history")
@@ -49,17 +27,27 @@ public class HistoryController {
     @Autowired
     private HistoryService historyService;
 
-    // POST /api/history/save
+    @Autowired
+    private MemberRepository memberRepository;
+    // POST /api/history/
     // 1. 사용자의 검색 키워드를 저장
+    
+    // HistoryDTO의 token에서 사용자 아이디를 추출하고(getclaims), 서비스 계층의 saveHistoryWithResults로 검색 기록을 result 테이블에 저장합니다.
+    // 성공 시 200 OK, 실패 시 500 Internal Server Error를 반환합니다.
     @PostMapping
-    public ResponseEntity<String> saveSearchKeyword(@RequestBody HistoryDTO h) {
+    public ResponseEntity<String> saveSearchKeyword(@RequestHeader("Authorization")String token, @RequestBody HistoryDTO h) {
     	System.out.println("history:" + h);
 
     	try {
-    		String token = h.getToken();
     		String username = JWTUtil.getClaim(token, "username");
-    		historyService.recordSearch(username, h);	// username, query를 db 테이블에 저장
+    		
+    		Member member = memberRepository.findByUsername(username)
+    				.orElseThrow(()-> new RuntimeException("유저 안뜸"));
+    		
+    		historyService.saveHistoryWithResults(h, member);
+    		
     		return ResponseEntity.ok("검색 기록이 저장되었습니다.");
+    		
     	} catch(Exception e) {
     		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
     							.body("저장 실패: " + e.getMessage());
@@ -68,16 +56,16 @@ public class HistoryController {
     
     // GET /api/history?userId=...
     // 2. 쿼리 파라미터로 userId를 받아서 히스토리 조회 (History 엔티티 그대로 반환 // dto 사용 불필요)
-    @GetMapping
-    public ResponseEntity<List<History>> getHistoryByUserId(@RequestParam String userId) {
-        
-        try {
-            List<History> list = historyService.getHistory(userId);
-            return ResponseEntity.ok(list);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(null);
-        }
-    }
+//    @GetMapping
+//    public ResponseEntity<List<History>> getHistoryByUserId(@RequestParam String userId) {
+//        
+//        try {
+//            List<History> list = historyService.getHistory(userId);
+//            return ResponseEntity.ok(list);
+//        } catch (Exception e) {
+//            return ResponseEntity.badRequest().body(null);
+//        }
+//    }
     
     // POST /api/history/read
     // 3. 토큰 기반 조회
