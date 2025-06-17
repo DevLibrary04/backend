6e3dbe55702b5a3115635dba06e10b1a8220bb4f
library04 <w1e1007@naver.com>
2025-06-17 11:14:19 +0900

    0617 db연동

--- src/main/java/edu/pnu/dto/HistoryDTO.java
+++ src/main/java/edu/pnu/dto/HistoryDTO.java
@@ -16,5 +16,7 @@ public class HistoryDTO {
 	private String userId;
 	private String query;
 	private List<ResultDTO> results = new ArrayList<>();
-	private String token; // JWT 토큰 필드 추가
+	private String token; 		// JWT 토큰 필드 추가
+	private String username;	// username 필드 추가
+	
 }
